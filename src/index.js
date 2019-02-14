979fe56105786c54f1e720aab6cea552b477043e
Kuferl <kuferl@kuferl.eu>
2019-02-14 20:52:20 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
+
+const LCUConnector = require('lcu-connector');
+const connector = new LCUConnector();
+
+const Store = require('electron-store');
+const store = new Store();
+
+const RiotWSProtocol = require('./lib/RiotWSProtocol');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -14,6 +22,7 @@ const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    fullscreenable: false,
   });
 
   // and load the index.html of the app.
@@ -34,7 +43,10 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+    createWindow();
+    connector.start();
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -52,6 +64,42 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+connector.on('connect', (data) => {
+    const ws = new RiotWSProtocol('wss://'+data.username+':'+data.password+'@'+data.address+':'+data.port+'/');
+    ws.on('open', () => {
+        ws.subscribe('OnJsonApiEvent', (data) => {
+            if(data.uri.includes('/lol-gameflow/v1/session')){
+                if(data.data.gameClient !== undefined){
+                    let scenes = store.get('scenes');
+                    if(data.data.gameClient.running) {
+                        mainWindow.webContents.send('active-scene', scenes.ingame)
+                    }else {
+                        mainWindow.webContents.send('active-scene', scenes.queue)
+                    }
+                }
+            }
+        });
+    });
+});
+
+ipcMain.on('update-storage', (event, arg) => {
+  store.set('scenes', arg);
+  event.returnValue = store.get('scenes');
+})
+
+ipcMain.on('get-storage', (event, arg) => {
+    let scenes = store.get('scenes');
+
+    if(!scenes){
+        scenes = {
+            queue: '',
+            ingame: ''
+        }
+    }
+
+    event.returnValue = scenes;
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
